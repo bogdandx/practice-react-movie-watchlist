5764252cf461fd90b4344ffc614a4c5c62aedcc8
bogdan <bogdandumitrescu@yahoo.com>
2024-03-20 20:18:24 -0600

    Extracted reusable component CollapsibleBox as the functionality was very similar between the list of movies and the list of watched movies.

--- src/MoviesListBox.js
+++ src/MoviesListBox.js
@@ -1,15 +1,8 @@
-import {useState} from "react";
 import MoviesList from "./MoviesList";
+import CollapsibleBox from "./CollapsibleBox";
 
 export default function MoviesListBox({movies}) {
-    const [isOpen1, setIsOpen1] = useState(true);
-
-    return <div className="box">
-        <button
-            className="btn-toggle"
-            onClick={() => setIsOpen1((open) => !open)}>
-            {isOpen1 ? "–" : "+"}
-        </button>
-        {isOpen1 && <MoviesList movies={movies}/>}
-    </div>;
+    return <CollapsibleBox>
+        <MoviesList movies={movies}/>
+    </CollapsibleBox>
 }
--- src/WatchedMoviesListBox.js
+++ src/WatchedMoviesListBox.js
@@ -1,6 +1,7 @@
 import {useState} from "react";
 import WatchedSummary from "./WatchedSummary";
 import WatchedMoviesList from "./WatchedMoviesList";
+import CollapsibleBox from "./CollapsibleBox";
 
 const tempWatchedData = [
     {
@@ -28,20 +29,10 @@ const tempWatchedData = [
 export default function WatchedMoviesListBox() {
     const [watched, setWatched] = useState(tempWatchedData);
 
-    const [isOpen2, setIsOpen2] = useState(true);
-
-    return <div className="box">
-        <button
-            className="btn-toggle"
-            onClick={() => setIsOpen2((open) => !open)}
-        >
-            {isOpen2 ? "–" : "+"}
-        </button>
-        {isOpen2 && (
-            <>
-                <WatchedSummary watched={watched}/>
-                <WatchedMoviesList watched={watched}/>
-            </>
-        )}
-    </div>;
+    return <CollapsibleBox>
+        <>
+            <WatchedSummary watched={watched}/>
+            <WatchedMoviesList watched={watched}/>
+        </>
+    </CollapsibleBox>
 }
